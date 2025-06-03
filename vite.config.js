1efab4372ca7a282121eb4b911a84f64d6fe0284
AYYAPPARAYUDU <ayyapparayudu.ravuvari@gmail.com>
2025-06-03 11:24:36 +0530

    Prepare for GitHub Pages deployment with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/REACT-LANDING-PAGE/',
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+      output: {
+        entryFileNames: `main.js`, // Force consistent name
+      },
+    },
+  },
 })
